66747bd3e2edb35717d0c6a6fa5dda61da520269
fjbasualdo <fjbasualdo@miuandes.cl>
2017-06-17 20:52:42 -0400

    boton
    agregue el boton q faltaba

---
+++ Nueva carpeta/EntregaFinalPOOO/SaveLoad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace EntregaFinalPOOO
+{
+    class SaveLoad
+    {
+        public Heroe h1;
+        public Heroe h2;
+        public minion m1;
+        public minion m2;
+
+    }
+}
